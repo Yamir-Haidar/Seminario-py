7be84878ed860d0ead6ef3d7ee3f4026f3bf6fa9
Yamir Haidar <yamirhaidar261@gmail.com>
2023-03-16 22:44:58 -0400

    Agregados comentarios y documentacion de las funciones

--- logic/Functions.py
+++ logic/Functions.py
@@ -1,7 +1,14 @@
 import matplotlib.pyplot as graph
+from logic.CaseRecord import CaseRecord
 
 
-def make_dictionary_communes_cases(record) -> dict:
+def make_dictionary_communes_cases(record: CaseRecord) -> dict:
+    """
+    Devuelve un diccionario con los nombres de las comunas como clave
+    del diccionario y el total de casos de esa comuna como valor.
+    :param record: CaseRecord
+    :return: dict
+    """
     dictionary = {}
     for person in record.records:
         if person.commune in dictionary:
@@ -11,7 +18,13 @@ def make_dictionary_communes_cases(record) -> dict:
     return dictionary
 
 
-def make_graph(dictionary):
+def make_graph(dictionary: dict) -> None:
+    """
+    Muestra un grafico de barras horizontales mediante SciView tomando
+    como valores de (x) los valores de las claves del diccionario(y).
+    :param dictionary: dict
+    :return: None
+    """
     graph.figure(figsize=(10, 6))
     graph.barh(list(dictionary.keys()), list(dictionary.values()))
     graph.title("Casos de COVID por comuna")
@@ -20,7 +33,13 @@ def make_graph(dictionary):
     graph.show()
 
 
-def predominate_omicron(record) -> dict:  # Comunas en las que predomina la variante omicron.
+def predominate_omicron(record: CaseRecord) -> dict:  # Comunas en las que predomina la variante omicron.
+    """
+    Devuelve un diccionario con las comunas donde predomina el omicron
+    como clave y la cantidad de casos de omicron como valor
+    :param record: CaseRecord
+    :return: dict
+    """
     result = {}
     for key, value in make_dictionary_communes_variant(record).items():
         if omicron_has_higher_frequency(value):
@@ -28,7 +47,13 @@ def predominate_omicron(record) -> dict:  # Comunas en las que predomina la vari
     return result
 
 
-def make_dictionary_communes_variant(record) -> dict:
+def make_dictionary_communes_variant(record: CaseRecord) -> dict:
+    """
+    Devuelve un diccionario con las comunas como clave y los tipos de
+    variantes presentes como valor
+    :param record: CaseRecord
+    :return: dict
+    """
     dictionary = {}
     for person in record.records:
         if not person.commune in dictionary.keys():
@@ -39,6 +64,13 @@ def make_dictionary_communes_variant(record) -> dict:
 
 
 def omicron_has_higher_frequency(string: str) -> bool:
+    """
+    Devuelve True si el string "omicron" es el que sucede mas veces en
+    el string introducido por parametros(En caso de empate con otro
+    string en mayor numero de repeticiones, la funcion devolvera False).
+    :param string: str
+    :return: bool
+    """
     switch_case = {}
     for word in string.split(" - "):
         if word in switch_case:
@@ -50,5 +82,10 @@ def omicron_has_higher_frequency(string: str) -> bool:
 
 
 def print_dictionary(dictionary: dict) -> None:
+    """
+    Imprime por pantalla el diccionario introducido por parametros
+    :param dictionary: dict
+    :return: None
+    """
     for key, value in dictionary.items():
         print(key, value)
--- logic/Main.py
+++ logic/Main.py
@@ -1,12 +1,23 @@
 from logic.CaseRecord import CaseRecord
 from logic.Functions import make_graph, make_dictionary_communes_cases, predominate_omicron, print_dictionary
 
+# Creo la clase que contiene los registros
 case_record = CaseRecord("../casosCovid.txt")
+
+# Creo un diccionario que contiene los casos por comunas
 cases_by_commune = make_dictionary_communes_cases(case_record)
 print("Cantidad de casos por comuna:")
+
+# Imprimo la informacion
 print_dictionary(cases_by_commune)
-print("\n********************************************************\n")
+
+# Imprimo el grafico de barras horizontales
 make_graph(cases_by_commune)
+print("\n********************************************************\n")
+
+# Creo un diccionario que contiene las comunas donde predomina el omicron
 communes_predominate_omicron = predominate_omicron(case_record)
 print("Comunas donde predomina la variante omicron:")
+
+# Imprimo la informacion
 print_dictionary(communes_predominate_omicron)
