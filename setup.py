b3b37fef053ab8d6f45d137a98d75a9b84108f29
Yamir Haidar <112997241+Yamir-Haidar@users.noreply.github.com>
2023-03-19 02:00:58 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
 setup(
-    name='nombre_del_proyecto',
+    name='Seminario-py',
     version='0.1',
     packages=find_packages(),
     install_requires=requirements,
