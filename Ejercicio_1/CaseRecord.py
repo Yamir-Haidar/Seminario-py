041d75d0739a2a0cd270afc9fa5913574a5a04b3
Yamir Haidar <yamirhaidar261@gmail.com>
2023-03-19 18:24:09 -0400

    Arreglado funcion fuera de clase

--- Ejercicio_1/CaseRecord.py
+++ Ejercicio_1/CaseRecord.py
@@ -1,35 +1,35 @@
 from Person import Person
 
 
-def __omicron_has_higher_frequency__(string: str) -> bool:
-    """
-    Devuelve True si el string "omicron" es el que sucede mas veces en
-    el string introducido por parametros(En caso de empate con otro
-    string en mayor numero de repeticiones, la funcion devolvera False).
-    :param string: str
-    :return: bool
-    """
-    switch_case = {}
-    for word in string.split(" - "):
-        if word in switch_case:
-            switch_case[word] += 1
-        else:
-            switch_case[word] = 1
-    return max(switch_case, key=switch_case.get) == "omicron" and \
-        list(switch_case.values()).count(switch_case["omicron"]) == 1
-
-
 class CaseRecord:
     def __init__(self, filename):
         self.records = []                # Registro de personas con los casos
         self.__load_file__(filename)     # Cargar el fichero al registro
 
-    def __load_file__(self, filename):
+    def __load_file__(self, filename: str) -> None:
         with open(filename, "r", encoding="utf-8") as file:
             for line in file:
                 data = line.strip().split(',')
                 record = Person(data[0], data[1], data[2], data[3])
                 self.records.append(record)
+
+    @staticmethod
+    def __omicron_has_higher_frequency__(string: str) -> bool:
+        """
+        Devuelve True si el string "omicron" es el que sucede mas veces en
+        el string introducido por parametros(En caso de empate con otro
+        string en mayor numero de repeticiones, la funcion devolvera False).
+        :param string: str
+        :return: bool
+        """
+        switch_case = {}
+        for word in string.split(" - "):
+            if word in switch_case:
+                switch_case[word] += 1
+            else:
+                switch_case[word] = 1
+        return max(switch_case, key=switch_case.get) == "omicron" and \
+            list(switch_case.values()).count(switch_case["omicron"]) == 1
 
     def make_dictionary_communes_cases(self) -> dict:
         """
@@ -37,13 +37,13 @@ class CaseRecord:
         del diccionario y el total de casos de esa comuna como valor.
         :return: dict
         """
-        dictionary = {}
+        result = {}
         for person in self.records:
-            if person.commune in dictionary:
-                dictionary[person.commune] += 1
+            if person.commune in result:
+                result[person.commune] += 1
             else:
-                dictionary[person.commune] = 1
-        return dictionary
+                result[person.commune] = 1
+        return result
 
     def predominate_omicron(self) -> dict:  # Comunas en las que predomina la variante omicron.
         """
@@ -53,7 +53,7 @@ class CaseRecord:
         """
         result = {}
         for key, value in self.make_dictionary_communes_variant().items():
-            if __omicron_has_higher_frequency__(value):
+            if self.__omicron_has_higher_frequency__(value):
                 result[key] = str(value).count("omicron")
         return result
 
@@ -63,20 +63,20 @@ class CaseRecord:
         variantes presentes como valor
         :return: dict
         """
-        dictionary = {}
+        result = {}
         for person in self.records:
             if commune is None:
-                if not person.commune in dictionary.keys():
-                    dictionary[person.commune] = str(person.variant_type)
+                if not person.commune in result.keys():
+                    result[person.commune] = str(person.variant_type)
                 else:
-                    dictionary[person.commune] += " - " + str(person.variant_type)
+                    result[person.commune] += " - " + str(person.variant_type)
             else:
                 if person.commune == commune:
-                    if person.commune in dictionary:
-                        dictionary[person.commune] += " - " + person.variant_type
+                    if person.commune in result:
+                        result[person.commune] += " - " + person.variant_type
                     else:
-                        dictionary[person.commune] = person.variant_type
-        return dictionary
+                        result[person.commune] = person.variant_type
+        return result
 
     def percentage_variant(self, commune: str) -> dict:
         """
@@ -85,18 +85,18 @@ class CaseRecord:
         :param commune: str
         :return: dict
         """
-        dictionary = self.make_dictionary_communes_variant(commune)
-        dictionary_helper = {}
-        variants = str(dictionary[commune]).split(" - ")
+        result = self.make_dictionary_communes_variant(commune)
+        result_helper = {}
+        variants = str(result[commune]).split(" - ")
         for string in variants:
-            if string in dictionary_helper:
-                dictionary_helper["Variante " + string] += 1
+            if string in result_helper:
+                result_helper["Variante " + string] += 1
             else:
-                dictionary_helper["Variante " + string] = 1
-        for key, value in dictionary_helper.items():
-            dictionary_helper[key] = str(round(value / len(variants) * 100, 2)) + "%"
-        dictionary[commune] = dictionary_helper
-        return dictionary
+                result_helper["Variante " + string] = 1
+        for key, value in result_helper.items():
+            result_helper[key] = str(round(value / len(variants) * 100, 2)) + "%"
+        result[commune] = result_helper
+        return result
 
     def total_vaccinated(self) -> int:
         """
